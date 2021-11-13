64779880f1f30612239acfca6adae4a5dbd82832
Dawid <d3seq2000@gmail.com>
2021-11-13 17:55:16 +0100

    Modified main.py

--- main.py
+++ main.py
@@ -5,22 +5,25 @@ Fs=2000
 tstep=1/Fs
 f0=100
 
-N=int(Fs/f0)
+N=int(10 * Fs/f0)
 
 t=np.linspace(0,(N-1)*tstep,N)
 fstep=Fs/N
 f=np.linspace(0,(N-1)*tstep,N)
 
-y=1*np.sin(2*np.pi*f0*t)
+y=1*np.sin(2*np.pi*f0*t) + 4*np.sin(3*np.pi*3*f0*t)
 
 
 #fft
-X=np.fft.fft(y)
-X_mag=np.abs(X)/N
+x=np.fft.fft(y)
+x_mag=np.abs(x)/N
 
+f_plot = f[0:int(N/2+1)]
+x_mag_plot = 2 * x_mag[0:int(N/2+1)]
+x_mag_plot[0] = x_mag[0] / 2
 
 #plot
 fig,[ax1,ax2]=plt.subplots(nrows=2,ncols=1)
 ax1.plot(t,y,'.-')
-ax2.plot(f,X_mag,'.-')
+ax2.plot(f_plot,x_mag_plot,'.-')
 plt.show()
